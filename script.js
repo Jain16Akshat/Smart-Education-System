d6f52750b711ec7e57be2dfdb65282ba6ec5fd92
Subhajit-sahu <subhajitsahu133@gmail.com>
2024-10-04 11:33:47 -1200

    msh=g

--- script.js
+++ script.js
@@ -101,3 +101,24 @@ document.addEventListener("DOMContentLoaded", function() {
       }
   });
 });
+
+// View More and Less Changes
+
+function toggleText(element){
+  let parent = element.parentElement;
+  let dots = parent.querySelector(".dots");
+  let more = parent.querySelector(".more");
+
+  // view less situation
+  if(dots.style.display === "none"){
+    element.innerHTML = "View More →";
+    dots.style.display = "inline";
+    more.style.display = "none";
+  }
+  // view more situation
+  else{
+    element.innerHTML = "View Less";
+    dots.style.display = "none";
+    more.style.display = "inline"
+  }
+}
